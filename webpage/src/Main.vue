ebb83ceeb6b459d407d8d03af05f35b7512408e9
vincent <vincent@cw.local>
2018-09-29 11:35:32 +0800

    update

--- webpage/src/Main.vue
+++ webpage/src/Main.vue
@@ -24,6 +24,7 @@
           </div>
         </div>
         <div class="header-avator-con">
+          <a href="/static/tutorial.pdf" target="_blank">使用教程</a>
           <div @click="handleFullScreen" v-if="showFullScreenBtn" class="full-screen-btn-con">
             <Tooltip :content="isFullScreen ? '退出全屏' : '全屏'" placement="bottom">
               <Icon :type="isFullScreen ? 'arrow-shrink' : 'arrow-expand'" :size="23"></Icon>
