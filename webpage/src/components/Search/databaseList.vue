8a41dd23666f86978a7ed39964b32e0eac0b2e93
vincent <vincent@cw.local>
2018-10-07 14:36:25 +0800

    up

--- webpage/src/components/Search/databaseList.vue
+++ webpage/src/components/Search/databaseList.vue
@@ -58,20 +58,109 @@
           },
           {
             title: '环境',
-            key: 'computer_room'
+            key: 'computer_room',
+            width: 150
           },
           {
             title: '数据库地址',
-            key: 'ip'
+            key: 'ip',
+            width: 400
           },
           {
             title: '端口',
-            key: 'port'
+            key: 'port',
+            width: 50,
+            render: (h, params) => {
+              return h('div', [
+                h('Button', {
+                  props: {
+                    size: 'small',
+                    type: 'info'
+                  },
+                  on: {
+                    click: () => {
+                      this.edit_tab(params.index)
+                    }
+                  }
+                }, '查看信息'),
+                h('Button', {
+                  style: {
+                    marginLeft: '8px'
+                  },
+                  props: {
+                    type: 'success',
+                    size: 'small'
+                  },
+                  on: {
+                    click: () => {
+                      this.dingding(params.row)
+                    }
+                  }
+                }, '钉钉推送信息'),
+                h('Button', {
+                  style: {
+                    marginLeft: '8px'
+                  },
+                  props: {
+                    type: 'warning',
+                    size: 'small'
+                  },
+                  on: {
+                    click: () => {
+                      this.deldatabases(params.index)
+                    }
+                  }
+                }, '删除')
+              ])
+            }
           }
         ],
         rowdata: [],
         modal: false,
         // 添加数据库信息
+        formItem: {
+          name: '',
+          ip: '',
+          add: '',
+          username: '',
+          password: '',
+          port: ''
+        },
+        // 添加表单验证规则
+        ruleInline: {
+          name: [{
+            required: true,
+            message: '请填写业务信息',
+            trigger: 'blur'
+          }],
+          ip: [{
+            required: true,
+            message: '请填写连接地址',
+            trigger: 'blur'
+          }],
+          username: [{
+            required: true,
+            message: '请填写用户名',
+            trigger: 'blur'
+          }],
+          port: [{
+            required: true,
+            message: '请填写端口',
+            trigger: 'blur'
+          }],
+          password: [{
+            required: true,
+            message: '请填写密码',
+            trigger: 'blur'
+          }]
+        },
+        // 生成字典规则
+        dataset: [],
+        Generate: {
+          textarea: '',
+          add: '',
+          name: ''
+        },
         dictionary: {
           name: '',
           add: '',
@@ -82,10 +171,114 @@
           getdellist: [],
           getdel: [],
           delname: ''
-        }
+        },
+        delbaseModal: false,
+        delbasename: '',
+        delconfirmbasename: '',
+        pagenumber: 1,
+        addDingding: false,
+        dingdingbeforetext: '',
+        dingdingaftertext: '',
+        dingname: '',
+        dingdingid: null,
+        tmp_id: null,
+        diclist: [],
+        baseinfo: false,
+        editbaseinfo: {}
       }
     },
     methods: {
+      del () {
+        this.modal = false
+        this.formItem = {}
+      },
+      testlink () {
+        axios.put(util.url + '/management_db/test', {
+          'ip': this.formItem.ip,
+          'user': this.formItem.username,
+          'password': this.formItem.password,
+          'port': this.formItem.port
+        })
+          .then(res => {
+            util.notice(res.data)
+          })
+          .catch(error => {
+            util.err_notice(error)
+          })
+      },
+      add () {
+        for (let i of this.rowdata) {
+          if (i.connection_name === this.formItem.name) {
+            util.err_notice('业务信息重复,请更改为其他!')
+            return
+          }
+        }
+        this.$refs['formValidate'].validate((valid) => {
+          if (valid) {
+            let data = {
+              'connection_name': this.formItem.name,
+              'ip': this.formItem.ip,
+              'computer_room': this.formItem.add,
+              'username': this.formItem.username,
+              'password': this.formItem.password,
+              'port': this.formItem.port
+            }
+            axios.post(util.url + '/management_db/', {
+              'data': JSON.stringify(data)
+            })
+              .then(() => {
+                util.notice('数据库信息添加成功,请对相应用户赋予该数据库访问权限!')
+                this.$refs.totol.currentPage = 1
+                this.mountdata()
+              })
+              .catch(error => {
+                util.err_notice(error)
+              })
+            this.del()
+          }
+        })
+      },
+      edit_tab (index) {
+        this.baseinfo = true
+        this.editbaseinfo = this.rowdata[index]
+      },
+      // 删除数据库
+      deldatabases (index) {
+        this.delbaseModal = true
+        this.delbasename = this.rowdata[index].connection_name
+      },
+      // 删除数据库字典
+      Deletedic () {
+        if (this.dictionary.delname.length === 0) {
+          this.$Message.error({
+            content: '请选择相应的数据库再删除!',
+            duration: 5
+          })
+        } else {
+          if (this.dictionary.getdel.length === 0) {
+            this.$Message.error({
+              content: '请选择相应的数据表再删除!',
+              duration: 5
+            })
+          } else {
+            this.$Loading.start()
+            axios.put(`${util.url}/adminsql/deldic`, {
+              'name': this.dictionary.delname,
+              'basename': this.dictionary.getdel
+            })
+              .then(res => {
+                util.notice(res.data)
+                this.$Loading.finish()
+                this.cleardata()
+              })
+              .catch(error => {
+                util.err_notice(error)
+                this.$Loading.error()
+              })
+          }
+        }
+      },
+      // 生成数据库字典
       Commit () {
         if (this.dictionary.databases.length === 0) {
           this.$Message.error({
@@ -138,9 +331,24 @@
           .catch(() => {
             util.err_notice('数据库信息获取失败,请检查网络状态.')
           })
-      }
-    },
-    getdiclist (val) {
+      },
+      // 全选
+      dicCheckAll () {
+        if (this.dictionary.indeterminate) {
+          this.dictionary.checkAll = false
+        } else {
+          this.dictionary.checkAll = !this.dictionary.checkAll
+        }
+        this.dictionary.indeterminate = false
+
+        if (this.dictionary.checkAll) {
+          this.dictionary.databases = this.dictionary.databasesList
+        } else {
+          this.dictionary.databases = []
+        }
+      },
+      // 生成已生成字典的库列表
+      getdiclist (val) {
         if (val.length === 0) {
           return
         }
@@ -154,6 +362,34 @@
             util.err_notice(error)
           })
       },
+      // 重置
+      cleardata () {
+        this.dictionary.name = ''
+        this.dictionary.databases = []
+        this.dictionary.databasesList = []
+        this.dictionary.getdellist = []
+        this.dictionary.getdel = []
+        this.dictionary.delname = ''
+      },
+      delbaselink () {
+        if (this.delbasename === this.delconfirmbasename) {
+          axios.delete(`${util.url}/management_db/${this.delbasename}`)
+            .then(res => {
+              util.notice(res.data)
+              this.delbaseModal = false
+              this.delconfirmbasename = ''
+              this.$refs.totol.currentPage = 1
+              this.mountdata()
+            })
+            .catch(error => {
+              util.err_notice(error)
+            })
+        } else {
+          this.$Message.error({
+            content: '请确认输入的业务信息一致！'
+          })
+        }
+      },
       mountdata (vl = 1) {
         axios.get(`${util.url}/management_db/all/?page=${vl}&permissions_type=base`)
           .then(res => {
@@ -166,7 +402,42 @@
             util.err_notice(error)
           })
       },
-      mounted () {
+      dingding (vl) {
+        this.dingname = vl.connection_name
+        axios.get(`${util.url}/dingding?connection_name=${this.dingname}`)
+          .then(res => {
+            this.dingdingid = res.data.id
+            this.dingdingbeforetext = res.data.before
+            this.dingdingaftertext = res.data.after
+          })
+          .catch(error => {
+            util.err_notice(error)
+          })
+        this.addDingding = true
+      },
+      savedingding () {
+        axios.post(`${util.url}/dingding/`, {
+          'before': this.dingdingbeforetext,
+          'after': this.dingdingaftertext,
+          'id': this.dingdingid
+        })
+          .then(() => {
+            util.notice('钉钉推送消息已设置成功!')
+            this.addDingding = false
+          })
+          .catch(error => {
+            util.err_notice(error)
+          })
+      },
+      update_base () {
+        axios.put(`${util.url}/management_db/update`, {
+          'data': JSON.stringify(this.editbaseinfo)
+        })
+          .then(res => util.notice(res.data))
+          .catch(err => util.err_notice(err))
+      }
+    },
+    mounted () {
       this.mountdata()
     }
   }
