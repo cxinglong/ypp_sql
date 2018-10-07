f4cc2c80b8fd0c759e9b0d8da287cf4640d39aa0
vincent <vincent@cw.local>
2018-10-07 14:16:53 +0800

    up

--- webpage/src/components/Search/databaseList.vue
+++ webpage/src/components/Search/databaseList.vue
@@ -20,7 +20,7 @@
             <div class="edittable-testauto-con">
               <Form :model="dictionary" :label-width="320" ref="generation">
                 <FormItem label="业务信息:" prop="dic">
-                  <Select v-model="dictionary.name" placeholder="请选择业务信息" style="width: 20%" @on-change="BaseList"
+                  <Select v-model="dictionary.name" placeholder="请选择业务信息" style="width: 25%" @on-change="BaseList"
                           transfer>
                     <Option v-for="i in rowdata" :value="i.id" :key="i.connection_name">{{i.connection_name}}</Option>
                   </Select>
@@ -40,6 +40,8 @@
 </template>
 <script>
   import '../../assets/tablesmargintop.css'
+  import axios from 'axios'
+  import util from '../../libs/util'
   import ICol from '../../../node_modules/iview/src/components/grid/col'
 
   export default {
@@ -82,6 +84,75 @@
           delname: ''
         }
       }
-    }
+    },
+    methods: {
+      Commit () {
+        if (this.dictionary.databases.length === 0) {
+          this.$Message.error({
+            content: '请选择相应的数据库再生成数据字典!',
+            duration: 5
+          })
+        } else {
+          this.$Spin.show({
+            render: (h) => {
+              return h('div', [
+                h('Icon', {
+                  props: {
+                    size: 30,
+                    type: 'ios-loading'
+                  },
+                  style: {
+                    animation: 'ani-demo-spin 1s linear infinite'
+                  }
+                }),
+                h('div', '数据库字典正在生成中,请稍后........')
+              ])
+            }
+          })
+          axios.put(`${util.url}/adminsql/Generation`, {
+            'id': this.tmp_id,
+            'basename': JSON.stringify(this.dictionary.databases)
+          })
+            .then(res => {
+              util.notice(res.data)
+              this.$Spin.hide()
+              this.cleardata()
+            }).catch(error => {
+            util.err_notice(error)
+            this.$Spin.hide()
+          })
+        }
+      },
+      // 生成数据库全部库名
+      BaseList (vl) {
+        if (vl.length === 0) {
+          return
+        }
+        this.tmp_id = vl
+        axios.put(`${util.url}/workorder/basename`, {
+          'id': vl
+        })
+          .then(res => {
+            this.dictionary.databasesList = res.data
+          })
+          .catch(() => {
+            util.err_notice('数据库信息获取失败,请检查网络状态.')
+          })
+      }
+    },
+    getdiclist (val) {
+        if (val.length === 0) {
+          return
+        }
+        axios.put(`${util.url}/sqldic/getdiclist`, {
+          'name': val
+        })
+          .then(res => {
+            this.dictionary.getdellist = res.data
+          })
+          .catch(error => {
+            util.err_notice(error)
+          })
+      }
   }
 </script>
